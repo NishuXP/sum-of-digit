52bd663fabe91129ecc3b6f87a3c76e9ec46cf95
Nishant Dhole <nishantdhole8@gmail.com>
2026-08-13 22:41:43 +0530

    Add files via upload

---
+++ Sum of digits.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, digit, sum = 0;
+
+    printf("Enter a positive number: ");
+    scanf("%d", &num);
+
+    do {
+        digit = num % 10;
+        sum += digit;
+        num /= 10;
+    } while (num > 0);
+
+    printf("Sum of digits = %d\n", sum);
+
+    return 0;
+}
